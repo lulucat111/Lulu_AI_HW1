eb8259956e36f5e668690ec582d98ff16548db3a
lulucat111 <yuruluo@usc.edu>
2016-08-31 22:03:07 -0500

    AI_homework1
    test test

---
+++ homework.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS561_homework
+//
+//  Created by yuruluo on 16/8/31.
+//  Copyright © 2016年 yuruluo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
